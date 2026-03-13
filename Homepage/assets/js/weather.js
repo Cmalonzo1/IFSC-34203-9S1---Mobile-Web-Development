6c5a5b66d3b50f31c36f119ca2abc1d3aa5e2fdf
Cmalonzo1 <142922371+Cmalonzo1@users.noreply.github.com>
2026-03-13 17:01:52 -0500

    api stuff

--- Homepage/assets/js/weather.js
+++ Homepage/assets/js/weather.js
@@ -1,34 +1,21 @@
-async function fetchWeather(){
-    const url = `https://api.open-meteo.com/v1/forecast?latitude=34.7253&longitude=-92.3379&daily=temperature_2m_min,temperature_2m_max,weather_code&timezone=America%2FChicago&forecast_days=1&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch`
+url_imperial = 'https://api.open-meteo.com/v1/forecast?latitude=34.7253&longitude=-92.3379&current=weather_code,temperature_2m,cloud_cover,is_day&timezone=America%2FChicago&forecast_days=1&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch&forecast_hours=1&past_hours=1'
 
+url_metric = 'https://api.open-meteo.com/v1/forecast?latitude=34.7253&longitude=-92.3379&current=weather_code,temperature_2m,cloud_cover,is_day&timezone=America%2FChicago&forecast_days=1&forecast_hours=1&past_hours=1'
+
+async function fetchWeather() {
+    const url = url_imperial;
     try{
         const response = await fetch(url);
-        if (!response.ok){
-            throw new Error(`Error ${response.status}`);
-        }
         const data = await response.json();
-        return data;
-    } catch (e){
-        console.error('Error fetching weather data: ', e);
-        return null;
-    }
-}
 
-function displayWeather(data) {
-    if (!data) {
-        document.querySelector('.weather').innerHTML = '<p>Failed to load weather</p>';
-        return;
+        
+
+        console.log(data.current.temperature_2m);
+    } catch (error){
+        console.error("Could not fetch weather data: ", error);
     }
 
-    const minTemp = data.current.temperature_2m_min;
-    const maxTemp = data.current.temperature_2m_max;
 
-    document.querySelector('.weather').innerHTML = `
-        <p>Temperature: ${minTemp} &#8594 ${maxTemp}</p>
-    `;
 }
 
-tener('DOMContentLoaded', async () => {
-    const weatherData = await fetchWeather();
-    displayWeather(weatherData);
-});
+fetchWeather();
