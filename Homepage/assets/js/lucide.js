6e3eca19d19eab6f598b53ae447b9667a6b995cf
Cmalonzo1 <142922371+Cmalonzo1@users.noreply.github.com>
2026-03-17 17:00:13 -0500

    a LOT

---
+++ Homepage/assets/js/lucide.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    lucide.createIcons({
+        attrs: {
+            class: ['weather-icon'],
+            stroke: '#fff'
+        },
+        nameAttr: 'data-lucide'
+    });
+});
--- Homepage/assets/js/weather.js
+++ Homepage/assets/js/weather.js
@@ -2,22 +2,15 @@ url_imperial = 'https://api.open-meteo.com/v1/forecast?latitude=34.7253&longitud
 
 url_metric = 'https://api.open-meteo.com/v1/forecast?latitude=34.7253&longitude=-92.3379&current=weather_code,temperature_2m,cloud_cover,is_day&timezone=America%2FChicago&forecast_days=1&forecast_hours=1&past_hours=1'
 
+const temp = document.getElementById('temperature');
+
 const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
-/*
-weatherCache = localStorage.getItem('weatherData');
-timeCache = localStorage.getItem('time');
-const currentTime = Date.now();
-*/
-
 async function fetchWeather(latitude, longitude) {
-    /*const url = url_imperial; */
     
-    const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=weather_code,temperature_2m,cloud_cover,is_day&timezone=America%2FChicago&forecast_days=1&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch&forecast_hours=1&past_hours=1`;
+    const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=weather_code,temperature_2m,cloud_cover,is_day&timezone=${encodeURIComponent(timezone)}&forecast_days=1&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch&forecast_hours=1&past_hours=1`;
 
-    const temp = document.getElementById('temperature');
-
-    try{
+    try{ 
         const response = await fetch(url);
         const data = await response.json();
         temp.innerHTML = Math.round(data.current.temperature_2m) + data.current_units.temperature_2m;
@@ -27,7 +20,6 @@ async function fetchWeather(latitude, longitude) {
     }
 }
 
-
 navigator.geolocation.getCurrentPosition((position) => {
 
     const {latitude, longitude} = position.coords;
@@ -36,7 +28,6 @@ navigator.geolocation.getCurrentPosition((position) => {
     fetchWeather(latitude, longitude);
 
 }, (error) => {
-    console.error('User denied access to geolocation. Displaying current campus weather');
     fetchWeather(34.7253, -92.3379);
 }
 ); 
